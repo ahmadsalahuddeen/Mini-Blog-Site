9641998bb2c8b14385541976e389e9b12583887b
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-06-03 15:12:24 +0530

    guery get and post service

--- query-service/index.js
+++ query-service/index.js
@@ -1,20 +1,28 @@
-const { default: axios } = require('axios');
-const express = require('express');
-const app = express()
-const cors = require('cors');
+const { default: axios } = require("axios");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+const post = {};
 
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
 
-const post = {}
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    post[id] = { id, title, comments: [] };
+  }
 
-app.post('/events', (req, res)=>{
-    
-})
-app.get('/posts', (req, res)=>{
+  if (type === "CommentCreated") {
+    const { id, postId, content } = data;
+    post[postId].comments.push({id, content}); // comments is an array of objects
+  }
+});
+app.get("/posts", (req, res) => {
+  res.send(post);
+});
 
-})
-
-app.listen(4003, ()=> console.log('server started listening on 4003'))
+app.listen(4003, () => console.log("server started listening on 4003"));
