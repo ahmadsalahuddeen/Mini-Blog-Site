ee4d000951b6df06fda2a930e375fb343a44fc3c
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-06-15 22:46:24 +0530

    event sync :+1

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,15 +6,20 @@ app.use(express.json())
 app.use(cors())
 
 
-
+const events = [];
 app.post('/events', async(req, res)=>{
     const event = req.body
+    events.push(event)
 
     await axios.post('http://localhost:4001/events', event);
     await axios.post('http://localhost:4002/events', event);
     await axios.post('http://localhost:4003/events', event);
     await axios.post('http://localhost:4009/events', event);
     res.send({status: 'OK'})
+})
+
+app.get('/events', (req, res)=>{
+    res.send(events)
 })
 
 
